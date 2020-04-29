c6a3cedb9e52562fca99e602f2d43a39bce3d57f
Cameronmbell99 <cameronbellbchs@gmail.com>
2020-04-28 22:59:06 -0700

    updated server.js file to work with heroku

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 //Dependencies
 var express = require("express");
 var path = require("path");
+var friends = require("./app/data/friends.js")
 
 // Setting up Express App
 var app = express();
-var PORT = 3000;
 
 // Handles Data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
-var tables = [{
-    customerName: "Justin",
-    customerPhone: 000 - 000 - 0000,
-    customerEmail: "the@something.com",
-    customerID: "something"
-}]
 
 //routes
 app.get("/", function(req, res) {
@@ -29,7 +21,7 @@ app.get("/survey", function(req, res) {
 
 //display arrays
 app.get("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "friends.js"));
+    return res.json(friends);
 });
 
 //starts the server
